6287206cb77fc1177804acfc9c8cd09c8c82073f
Aleksander Wieczarek <aleksander.wieczarek1@gmail.com>
2023-11-24 21:20:56 +0100

    add keycloak

--- src/Components/UserList.tsx
+++ src/Components/UserList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { chatService } from '../Services/chatService';
+import {
+    CircularProgress,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    TablePagination,
+    Checkbox,
+    TextField
+} from '@mui/material';
 type Props = {};
 interface User {
     id: string;
@@ -9,24 +23,44 @@ interface User {
 const UserList: React.FC = (props: Props) => {
     const { data: users, isLoading, isError, error } = useQuery<User[], Error>('users', () => chatService.getUsers());
 
-    if (isLoading) {
-        return <div>Ładowanie...</div>;
-    }
 
     if (isError) {
         return <div>Wystąpił błąd przy pobieraniu użytkowników: {error?.message}</div>;
     }
+    if (isLoading) {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <CircularProgress />
+            </div>
+        );
+    }
 
     return (
-        <div>
-            <h1>Lista Użytkowników</h1>
-            <ul>
-                {users?.map(user => (
-                    <li key={user.id}>{user.username}</li>
-                ))}
-            </ul>
+        <div style={{ padding: '20px' }}>
+            <Typography variant="h4" gutterBottom>
+                Lista Użytkowników
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>id</TableCell>
+                            <TableCell>Username</TableCell>
+                            {/* Add more header cells if needed */}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users?.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.username}</TableCell>
+                                {/* Add more cells for each user property */}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
-
 export default UserList;
--- src/Providers/AuthenticationProvider.tsx
+++ src/Providers/AuthenticationProvider.tsx
@@ -1,38 +1,44 @@
 import type {
-  AuthClientError,
-  AuthClientEvent,
-  AuthClientTokens,
+    AuthClientError,
+    AuthClientEvent,
+    AuthClientTokens,
 } from "@react-keycloak/core/lib/types";
-import { ReactKeycloakProvider } from "@react-keycloak/web";
+import {ReactKeycloakProvider} from "@react-keycloak/web";
 import keycloak from "../keycloak";
 import CircularProgress from "@mui/material/CircularProgress";
 
 type AuthenticationProviderProps = { children: React.ReactNode };
 
 const eventLogger = (eventType: AuthClientEvent, error?: AuthClientError) => {
-  console.info("onKeycloakEvent", eventType, error);
+    console.info("onKeycloakEvent", eventType, error);
 };
 
 const tokenLogger = (tokens: AuthClientTokens) => {
-  console.info("onKeycloakTokens", tokens);
+    console.info("onKeycloakTokens", tokens);
 };
 
 const initOptions = {
-  onLoad: "login-required",
+    onLoad: "login-required",
+    checkLoginIframe: false,
 };
 
-const AuthenticationProvider = ({ children }: AuthenticationProviderProps) => {
-  return (
-    <ReactKeycloakProvider
-      authClient={keycloak}
-      onEvent={eventLogger}
-      onTokens={tokenLogger}
-      initOptions={initOptions}
-      LoadingComponent={<CircularProgress size={120} />}
-    >
-      {children}
-    </ReactKeycloakProvider>
-  );
+const AuthenticationProvider = ({children}: AuthenticationProviderProps) => {
+    return (
+        <ReactKeycloakProvider
+            authClient={keycloak}
+            onEvent={eventLogger}
+            onTokens={tokenLogger}
+            initOptions={initOptions}
+            LoadingComponent={<CircularProgress size={120} style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: 'auto'
+            }}/>}
+        >
+            {children}
+        </ReactKeycloakProvider>
+    );
 };
 
 export default AuthenticationProvider;
